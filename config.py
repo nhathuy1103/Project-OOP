dcf35e6b3c1090110455fc1cb9f44fd85aae3a08
KienDoan-A11 <akd.vn1108@gmail.com>
2024-10-13 23:52:58 +0700

    them file requirements.txt va sua file config.py

--- config.py
+++ config.py
@@ -1,22 +1,18 @@
 class Config:
     # Thông tin cấu hình kết nối đến MySQL
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:none@localhost/quan_ly_ban_le'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql12736967:4LIpGVNBym@sql12.freemysqlhosting.net/sql12736967'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    
-
-
-
-# Kiểm tra kết nối (thay đổi các tham số để test trên máy mình nhé)
-'''
+# Kiểm tra kết nối
 import pymysql  # Thêm dòng này để import pymysql
 
 try:
     connection = pymysql.connect(
-        host='localhost',
-        user='root',
-        password='none',  # Thay 'none' bằng mật khẩu đúng nếu cần
-        db='quan_ly_ban_le'
+        host='sql12.freemysqlhosting.net',
+        user='sql12736967',
+        password='4LIpGVNBym',  # Thay 'none' bằng mật khẩu đúng nếu cần
+        db='sql12736967',
+        port=3306  # Cổng kết nối mặc định cho MySQL
     )
     with connection.cursor() as cursor:
         cursor.execute("SELECT VERSION()")
@@ -29,4 +25,3 @@ except Exception as e:
 finally:
     if 'connection' in locals() and connection:  # Kiểm tra xem connection đã được định nghĩa
         connection.close()
-'''
